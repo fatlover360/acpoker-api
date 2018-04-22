9bb3b1d67a329f055343238494995c3cf47b1bae
fatlover360 <joaocsvieira92@gmail.com>
2018-04-22 13:55:16 +0100

    d

---
+++ src/main/java/com/acpoker/acpokerapi/entity/Type.java
@@ -0,0 +1,40 @@
+package com.acpoker.acpokerapi.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "type_range")
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "color")
+    private String color;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
